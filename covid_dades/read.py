e2fd3f414554ed0e4ad625782f6566a9ea5bd89e
Denys Chorny <dech@dech-pop.home>
2022-02-05 02:13:41 +0100

    Made function that calculates the absolute diference and his percentage

--- covid_dades/read.py
+++ covid_dades/read.py
@@ -2,6 +2,7 @@
 # Suma total de los vacunados con la primera dosis y suma de todos los vacunados con la segunda
 
 from pathlib import Path
+import time
 
 text: str = Path("csv/covid-dades-aga.csv").read_text()
 
@@ -31,16 +32,39 @@ def sum_vacuna(table: list[list[str]], position: int) -> int:
 
     return result
 
+def absolute_dif(first_number: int, second_number: int) -> int:
+    result: int = 0
+    result_persentage: float = 0
 
-rows: list[str] = text_to_row(text)
-table: list[list[str]] = rows_to_table(rows)
-sum_vacuna_1: int = sum_vacuna(table, 20)
-sum_vacuna_2: int = sum_vacuna(table, 21)
-print("==================================")
-print(f"Suma de Primera Vacuna: {sum_vacuna_1}")
-print(f"Suma de Segunda Vacuna: {sum_vacuna_2}")
-print(f"Suma Total de Vacunats:", sum_vacuna_2 + sum_vacuna_1)
-print("==================================")
-print(f"Diferencia Absoluta: ", sum_vacuna_1 - sum_vacuna_2)
-print(f"Diferencia Absoluta en %: ")
-print("==================================")
+    if first_number > second_number:
+        result = first_number - second_number
+    else:
+        result = second_number - first_number
+
+    result_persentage = (result * 100) / first_number
+    return(result, result_persentage)
+
+def main():
+    start: float = time.time()
+
+    rows: list[str] = text_to_row(text)
+    table: list[list[str]] = rows_to_table(rows)
+    sum_vacuna_1: int = sum_vacuna(table, 20)
+    sum_vacuna_2: int = sum_vacuna(table, 21)
+
+    stop: float = time.time()
+
+    print(f"Tiempo Ejecucion: {stop-start}")
+
+    print("============================================")
+    print(f"Suma de Primera Vacuna: {sum_vacuna_1}")
+    print(f"Suma de Segunda Vacuna: {sum_vacuna_2}")
+    print(f"Suma Total de Vacunats:", sum_vacuna_2 + sum_vacuna_1)
+    print("============================================")
+
+    print(f"Diferencia Absoluta: ", absolute_dif(sum_vacuna_1, sum_vacuna_2)[0])
+    print(f"Diferencia Absoluta en Porcentaje: ", round(absolute_dif(sum_vacuna_1, sum_vacuna_2)[1], 2),"%")
+    print("============================================")
+
+if __name__ == "__main__":
+    main()
