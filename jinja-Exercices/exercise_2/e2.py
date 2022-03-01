a6a2a18817f2663c55a35ab2e8c23ffe46492ade
Denys Chorny <dech@dech-pop.home>
2022-03-01 17:56:15 +0100

    Files are now renamed perfectly, cleared the code

--- jinja-Exercices/exercise_2/e2.py
+++ jinja-Exercices/exercise_2/e2.py
@@ -30,21 +30,42 @@ Exercici 2:
 
 from pathlib import Path
 
-def rename_filename_list(old_filename_list: list[str], old_str: str, new_str: str) -> list[str]:
-    """Change values from a list"""
-    new_filename_list: list[str] = [filename.replace(old_str, new_str) for filename in old_filename_list]
+
+def rename_filename_on_disk(
+    old_filename_list: list[str], new_filename_list: list[str], dir: Path
+):
+    """Rename the files on the dist to a given list of names"""
+
+    new_dir: list[Path] = [dir / pattern for pattern in new_filename_list]
+    old_dir: list[Path] = [dir / pattern for pattern in old_filename_list]
+
+    for (old, new) in zip(old_dir, new_filename_list):
+        old.rename(new)
+
+
+def rename_filename_list(
+    old_filename_list: list[str], old_str: str, new_str: str
+) -> list[str]:
+    """Change old values from one list to another new ones"""
+    new_filename_list: list[str] = [
+        filename.replace(old_str, new_str) for filename in old_filename_list
+    ]
     return new_filename_list
+
 
 def rename_files(glob: str, old_str: str, new_str: str):
     """Rename files"""
     glob_dir: Path = Path(glob)
-    dir:      Path = glob_dir.parent
-    pattern:  Path = glob_dir.name
+    dir: Path = glob_dir.parent
+    pattern: Path = glob_dir.name
 
     old_filename_list: list[str] = [filepath.name for filepath in dir.glob(pattern)]
-    new_filename_list: list[str] = rename_filename_list(old_filename_list, old_str, new_str)
-    print(old_filename_list)
-    print(new_filename_list)
+    new_filename_list: list[str] = rename_filename_list(
+        old_filename_list, old_str, new_str
+    )
+
+    rename_filename_on_disk(old_filename_list, new_filename_list, dir)
+
 
 def main():
     rename_files("2-test-files/*.txt", "a", "b")
