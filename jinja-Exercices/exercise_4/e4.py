8c410a131172d90f4b82f5025d502e8704af51d8
Denys Chorny <dech@dech-pop.home>
2022-03-03 15:21:03 +0100

    update

--- jinja-Exercices/exercise_4/e4.py
+++ jinja-Exercices/exercise_4/e4.py
@@ -9,7 +9,7 @@ Exercici 4:
 
 from pathlib import Path
 import pprint as pp
-
+import table
 
 # -----------------------------------------------------------------------------
 def csv_to_text(csv_path: Path) -> str:
@@ -26,7 +26,7 @@ def csv_text_to_list(csv_text: str) -> list[str]:
 def csv_list_to_table(csv_list: list[str]) -> list[list[str]]:
     buffer: list[str]       = []
     table:  list[list[str]] = []
-    for row in csv_list[1:]:
+    for row in csv_list[:]:
         buffer = row.split(";")
         table.append(buffer)
     return table
@@ -35,23 +35,29 @@ def get_header(csv_list: list[str]) -> list[str]:
     header: list[str] = csv_list[0].split(";")
     return header
 
-def create_dictionary(header: list[str], table: list[list[str]]) -> dict:
-    csv_dict: dict = {}
-    for cols in header:
-        csv_dict[cols] = ""
+# def dict_keys(header: list[str]) -> dict:
+#     csv_dict: dict = {}
+#     for cols in header:
+#         csv_dict[cols] = ""
+#     return csv_dict
 
-    return csv_dict
+
+    return
 
 # -----------------------------------------------------------------------------
 # Main
 # -----------------------------------------------------------------------------
 if __name__ == "__main__":
-    csv_path: Path            = Path("covid-dades-simple.csv")
-    csv_text: str             = csv_to_text(csv_path)
-    csv_list: list[str]       = csv_text_to_list(csv_text)
-    header:   list[str]       = get_header(csv_list)
-    table:    list[list[str]] = csv_list_to_table(csv_list)
-    csv_dict: dict            = create_dictionary(header, table)
+    # csv_path:      Path            = Path("covid-dades-simple.csv")
+    # csv_text:      str             = csv_to_text(csv_path)
+    # csv_list:      list[str]       = csv_text_to_list(csv_text)
+    # header:        list[str]       = get_header(csv_list)
+    # table:         list[list[str]] = csv_list_to_table(csv_list)
+
+    covid_table: list[list[str]] = table.read_table("covid-dades-simple.csv")
+    header:     list[str]       = covid_table[0]
+    covid_dict: dict[str, list] = {column_name: table.get_column(covid_table, column_name) for column_name in header}
+    print(covid_dict)
     # print(csv_dict)
 
 # -----------------------------------------------------------------------------
